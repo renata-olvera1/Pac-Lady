03422d60d5239f6b1806d72cd946b4fa36a9e39b
Pbenro <a23310297@live.ceti.mx>
2024-11-26 21:57:34 -0600

    Segunda Version Diagrama UML

---
+++ src/ventana.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Crear una ventana de SFML
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Pac-Lady");
+
+    // Crear una forma circular de SFML
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Verificar si se ha cerrado la ventana
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Limpiar la ventana
+        window.clear();
+
+        // Dibujar la forma en la ventana
+        window.draw(shape);
+
+        // Mostrar la ventana
+        window.display();
+    }
+
+    return 0;
+}
