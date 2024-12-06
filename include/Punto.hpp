3f190b429b2b55f6caca3710c9055fe3f38cb849
Pbenro <a23310297@live.ceti.mx>
2024-12-06 14:02:52 -0600

    Alta de Clases y Comienzo de la Clase Punto

---
+++ include/Punto.hpp
@@ -0,0 +1,23 @@
+#ifndef PUNTO_HPP
+#define PUNTO_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Punto {
+    public:
+    
+        Punto::Punto(float x, float y, float radio) {
+        circulo.setRadius(radio);
+        circulo.setFillColor(sf::Color::White);
+        circulo.setPosition(x - radio, y - radio); // Centrar el círculo
+    }
+
+    void Punto::dibujar(sf::RenderWindow& ventana) {
+        ventana.draw(circulo);
+    }
+
+    private:
+        sf::CircleShape circulo;
+    };
+
+#endif // PUNTO_HPP
