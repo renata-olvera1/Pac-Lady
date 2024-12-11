98e241cf1f83f9417176cbde341728027b26c981
Pbenro <a23310297@live.ceti.mx>
2024-12-10 22:19:06 -0600

    Puntos version 1

--- src/juego.cpp
+++ src/juego.cpp
@@ -19,11 +19,32 @@ int main() {
     // Centrar el mapa en la ventana
     mapa.centrarEnVentana(window.getSize());
 
-    // Crear puntos en diferentes posiciones
-    vector<Punto> puntos = {
-        Punto(550, 798, 5), Punto(580, 798, 5), Punto(610, 798, 5),
-        Punto(485, 798, 5), Punto(455, 798, 5), Punto(425, 798, 5),
-        Punto(395, 798, 5) //Punto(370, 798, 5) //Punto(500, 798, 5)
+    // Puntos Linea A
+    vector<Punto> Linea_A = {
+        Punto(545, 798, 4), Punto(575, 798, 4), Punto(605, 798, 4), 
+        Punto (630, 798, 4), Punto(485, 798, 4), Punto(455, 798, 4),
+        Punto(425, 798, 4), Punto(395, 798, 4)
+    };
+
+    // Puntos Linea B
+    vector<Punto> Linea_B = {
+        Punto(395, 768, 4), Punto(395, 708, 4), Punto(395, 408, 4),
+        Punto(395, 678, 4), Punto(395, 648, 4), Punto(395, 618, 4),
+        Punto(395, 588, 4), Punto(395, 558, 4), Punto(395, 528, 4),
+        Punto(395, 498, 4), Punto(395, 468, 4), Punto(395, 438, 4),
+        Punto(395, 378, 4), Punto(395, 828, 4), Punto(395, 850, 4),
+    };
+
+    //vector<Punto> Linea_C = {
+
+    //};
+
+    vector<Punto> Linea_J = {
+        Punto(355, 748, 4), Punto(330, 748, 4), Punto(305, 748, 4),
+        Punto(305, 773, 4), Punto(305, 795, 4), Punto(330, 795, 4),
+        Punto(355, 795, 4), Punto(380, 748, 4), Punto(355, 828, 4),
+        Punto(355, 863, 4), Punto(380, 863, 4), Punto(330, 863, 4),
+        Punto(305, 863, 4)
     };
 
     // Crear a PacLady
@@ -58,14 +79,22 @@ int main() {
         window.clear();
         mapa.dibujar(window.getRenderWindow());
 
-        // Dibujar los puntos
-        for (auto &punto : puntos) {
+        // Dibujar lineas de puntos
+        for (auto &punto : Linea_A) {
             punto.dibujar(window.getRenderWindow());
         }
 
-        /*for (auto& punto : puntos) {
+        for (auto &punto : Linea_B) {
             punto.dibujar(window.getRenderWindow());
-        }*/
+        }
+
+        for (auto &punto : Linea_C) {
+            punto.dibujar(window.getRenderWindow());
+        }
+
+        for (auto &punto : Linea_J) {
+            punto.dibujar(window.getRenderWindow());
+        }
 
         pacLady.dibujar(window.getRenderWindow());
         window.display();
